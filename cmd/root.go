cca234fa255cbfcfd449bf36b4660181aefaa00a
Marcin Bilski <bilus@Marcins-MacBook-Pro.local>
2019-02-17 16:55:39 +0100

    Do not print error messages twice.

--- cmd/root.go
+++ cmd/root.go
@@ -51,7 +51,6 @@ to quickly create a Cobra application.`,
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
