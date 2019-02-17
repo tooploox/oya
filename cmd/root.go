6afe882988f44ff09d9b42adf2fb1bd1098dc7e4
Marcin Bilski <bilus@Marcins-MacBook-Pro.local>
2019-02-17 19:06:29 +0100

    Disable cobra config file + useless "toggle".

--- cmd/root.go
+++ cmd/root.go
@@ -72,11 +72,7 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.oya.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.oya.yaml)")
 }
 
 // initConfig reads in config file and ENV variables if set.
