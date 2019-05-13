c814389e9fb572c7d65763db9da4f91bddb3f32e
bart <bart.dy@gmail.com>
2019-05-13 12:39:21 +0200

    oya description

--- cmd/root.go
+++ cmd/root.go
@@ -37,16 +37,9 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "oya",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Oya is a task manager and runner",
+	Long: `Oya takes the pain out of bootstrapping new deployable
+projects with packaged boilerplate & scripts.`,
 	SilenceErrors: true,
 	SilenceUsage:  true,
 }
